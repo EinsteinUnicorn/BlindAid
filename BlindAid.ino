acb8ecb1535a72bab8e3bd7b02d3e146d90cf2d9
Becky Button <einsteinunicorn@gmail.com>
2017-04-21 17:54:02 -0400

    adding ino

---
+++ BlindAid.ino
@@ -0,0 +1,32 @@
+#include <Servo.h>
+Servo servo;
+
+int usTrig = 3;
+int usEcho = 4;
+int duration = 0;
+int distanceCM = 0;
+
+int scaledServoValue = 0;
+void setup() 
+{                
+servo.attach(9);
+pinMode(usTrig, OUTPUT);
+pinMode(usEcho, INPUT);
+}
+
+void loop() 
+{
+ digitalWrite(usTrig, LOW);
+ delayMicroseconds(5);
+ digitalWrite(usTrig, HIGH);
+ delayMicroseconds(10);
+ digitalWrite(usTrig, LOW);
+ 
+ duration = pulseIn(usEcho, HIGH);
+ 
+ distanceCM = (duration/2) / 29.1;
+ 
+ scaledServoValue = map(distanceCM, 3, 400, 0, 180);
+ 
+ servo.write(scaledServoValue);
+}
